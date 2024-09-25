7f0bbee9e1a95db4b30ebf22aa564890d5c2f1b1
ABHISHEK KUMAR <abhishekblog6315@gmail.com>
2024-09-25 12:42:45 +0530

    initiated

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+         <div className="flex justify-center pt-48">
+             <div className="text-5xl font-bold">
+               Build clone of 75per
+             </div>
+          
+         </div>
+            <div className="flex justify-center text-2xl font-bold text-white-500 pt-12">
+             <a 
+             className="border p-2 rounded"
+             href="https://75per.vercel.app/">Visite</a>
+             </div>
+    </div>
+  );
+}
